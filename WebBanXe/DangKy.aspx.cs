707c2e906220d4f56cb63687736dce016bc457af
TanHoang03 <148403683+TanHoang03@users.noreply.github.com>
2024-04-10 15:22:43 +0700

    Create DangKy.aspx.cs
    test thử

---
+++ WebBanXe/DangKy.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebBanXe.Models;
+
+namespace WebBanXe
+{
+    public partial class DangKy : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void btnSigUp_Click(object sender, EventArgs e)
+        {
+         String chuoi_ket_noi = configurationManager.ConnectionStrings["WebBanXeConnectionStrings"].ConnectionStrings
+         sqlConnection conn = new SqlConnecion(chuoi_ket_noi);
+         conn.open();
+            
+        }
+    }
+}
